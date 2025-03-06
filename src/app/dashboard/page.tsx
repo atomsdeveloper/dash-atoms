6a165514dd83a16fd900daf4e8652a7212be5085
Renan Nascimento <atomsfrontend@gmail.com>
2025-03-05 23:56:55 -0300

    feat: add toggle language

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -28,23 +28,27 @@ import {
 
 import { Sun, Moon } from "lucide-react";
 import { ThemeContext } from "@/context/theme";
+import { useTranslation } from "next-i18next";
+import { useRouter } from "next/router";
 
 const DashboardPage = () => {
+  const { i18n } = useTranslation();
+  const router = useRouter();
   const [language, setLanguage] = React.useState("pt-br");
   const { theme, handleSetTheme } = React.useContext(ThemeContext);
 
   React.useEffect(() => {
-    document.documentElement.lang = language;
-  }, [language]);
+    i18n.changeLanguage(language);
+    router.push(router.asPath, undefined, { locale: language });
+  }, [language, i18n, router]);
 
-  
   return (
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
         <header className="flex h-16 items-center justify-between gap-2 py-2 pr-6 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <div className="flex h-full w-3/6 items-center gap-2 px-4">
-            <SidebarTrigger className={`-ml-1 ${theme? "text-white" : ""}`}/>
+            <SidebarTrigger className={`-ml-1 ${theme ? "text-white" : ""}`} />
             <Separator orientation="vertical" className="mr-2 h-4" />
             <Breadcrumb>
               <BreadcrumbList>
@@ -55,16 +59,16 @@ const DashboardPage = () => {
             </Breadcrumb>
           </div>
           <div className="flex h-full w-3/6 items-center justify-end gap-3">
-            <Button 
-              className={`w-16 h-8 rounded-full text-xs flex  ${theme ? "justify-end" : "justify-start"}`} 
+            <Button
+              className={`flex h-8 w-16 rounded-full text-xs ${theme ? "justify-end" : "justify-start"}`}
               onClick={() => handleSetTheme()}
             >
-              {theme ? <Moon size={10} /> : <Sun size={10}/> }
+              {theme ? <Moon size={10} /> : <Sun size={10} />}
             </Button>
 
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
-                <Button className="w-16 h-8 rounded-full text-xs">
+                <Button className="h-8 w-16 rounded-full text-xs">
                   {language}
                 </Button>
               </DropdownMenuTrigger>
@@ -75,10 +79,10 @@ const DashboardPage = () => {
                   value={language}
                   onValueChange={setLanguage}
                 >
-                  <DropdownMenuRadioItem value="en-US">
+                  <DropdownMenuRadioItem value="en">
                     Inglês
                   </DropdownMenuRadioItem>
-                  <DropdownMenuRadioItem value="pt-BR">
+                  <DropdownMenuRadioItem value="pt">
                     Português
                   </DropdownMenuRadioItem>
                 </DropdownMenuRadioGroup>
@@ -87,7 +91,7 @@ const DashboardPage = () => {
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min"> 
+          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
             <div className="grid auto-rows-min gap-4 md:grid-cols-2">
               <div className="aspect-video rounded-xl bg-muted/100" />
               <div className="aspect-video rounded-xl bg-muted/100" />
