1dfc8b68d02690fd35d36f2f8bd82f1f764401b9
Renan Nascimento <atomsfrontend@gmail.com>
2025-03-07 22:25:20 -0300

    chore: change color

--- src/app/dashboard/components/products-sold.tsx
+++ src/app/dashboard/components/products-sold.tsx
@@ -2,8 +2,8 @@ import { Tag } from "lucide-react";
 
 const ProductsSales = () => {
   return (
-    <div className="flex aspect-video flex-col gap-1 rounded-xl bg-green-200 bg-muted/50 p-4">
-      <div className="flex h-8 w-8 items-center justify-center rounded-full bg-green-400">
+    <div className="flex aspect-video flex-col gap-1 rounded-xl bg-green-300 bg-muted/50 p-4">
+      <div className="flex h-8 w-8 items-center justify-center rounded-full bg-green-500">
         <Tag size={18} color="white" />
       </div>
       <h1 className="text-2xl">68</h1>
